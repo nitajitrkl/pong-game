6c2e6e5feea6b8f32ee6e3238af1a09997a88d36
Ajit Behera <41351154+nitajitrkl@users.noreply.github.com>
2021-11-24 14:00:47 +0530

    v2.04

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -7,8 +9,9 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-l_paddle = Paddle((350, 0))
-r_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -18,6 +21,8 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 screen.exitonclick()
