967b839e16b1f9c0e9232e5fd5558a53587b32a0
Ajit Behera <41351154+nitajitrkl@users.noreply.github.com>
2021-11-24 13:53:56 +0530

    v2.02

--- main.py
+++ main.py
@@ -10,25 +10,14 @@ screen.tracer(0)
 l_paddle = Paddle((350, 0))
 r_paddle = Paddle((-350, 0))
 
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
     screen.update()
-
-# def go_up():
-#     new_y = paddle.ycor() + 20
-#     paddle.goto(paddle.xcor(), new_y)
-#
-#
-# def go_down():
-#     new_y = paddle.ycor() - 20
-#     paddle.goto(paddle.xcor(), new_y)
-
-
-screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
-
-
 
 screen.exitonclick()
